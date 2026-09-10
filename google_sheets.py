363de48c7964bbaf53e6ffe08a51afc3111a68a5
Andruoff <andrew.ruoff.37@gmail.com>
2026-09-10 11:56:22 -0400

    Sheets have been connected!

--- google_sheets.py
+++ google_sheets.py
@@ -2,8 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # Load Google Sheets API credentials
-SERVICE_ACCOUNT_FILE = "wb3-tts-importer-416c2c5a00d4.json"  # Update with your JSON file
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SERVICE_ACCOUNT_FILE = "credentials.json"  # Update with your JSON file
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
 # Authenticate and create the client
 creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
@@ -11,3 +11,5 @@ client = gspread.authorize(creds)
 
 SHEET_ID = "1RyVuXuTbjReXnPB0BYGq-aMEtmI_Z3HK2iEkngKHFN4"  # Replace with your actual sheet ID
 sheet = client.open_by_key(SHEET_ID).sheet1  # Access the first worksheet
+
+print(sheet.row_values(1))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gspread
-#import pillow
+import PIL
 
 def main():
     print("Hi")
