685ef5f71ab8a74e6eca81b82cc459c163c5e90e
Andruoff <andrew.ruoff.37@gmail.com>
2026-09-10 12:27:23 -0400

    Deleted a thing.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import gspread
 import PIL
 
+CARD_WIDTH = 250
+CARD_HEIGHT = 350
+
+def generate_card(card_data: list):
+    #new_card = PIL.Image.new("hsv", (CARD_WIDTH, CARD_HEIGHT), (255, 255, 255))
+    #new_card.show()
+    pass
+
 def main():
     print("Hi")
+    generate_card([1])
 
 if __name__ == '__main__':
     main()
