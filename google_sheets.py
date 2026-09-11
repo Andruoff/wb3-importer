e598c5575d43018e0634d8b138f8c5493af9d6d5
Andruoff <andrew.ruoff.37@gmail.com>
2026-09-11 14:25:35 -0400

    Google sheets reconnected

--- google_sheets.py
+++ google_sheets.py
@@ -1,15 +1,15 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
 
-# Load Google Sheets API credentials
-SERVICE_ACCOUNT_FILE = "credentials.json"  # Update with your JSON file
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+# # Load Google Sheets API credentials
+# SERVICE_ACCOUNT_FILE = "credentials.json"  # Update with your JSON file
+# SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
-# Authenticate and create the client
-creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-client = gspread.authorize(creds)
+# # Authenticate and create the client
+# creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# client = gspread.authorize(creds)
 
-SHEET_ID = "1RyVuXuTbjReXnPB0BYGq-aMEtmI_Z3HK2iEkngKHFN4"  # Replace with your actual sheet ID
-sheet = client.open_by_key(SHEET_ID).sheet1  # Access the first worksheet
+# SHEET_ID = "1RyVuXuTbjReXnPB0BYGq-aMEtmI_Z3HK2iEkngKHFN4"  # Replace with your actual sheet ID
+# sheet = client.open_by_key(SHEET_ID).sheet1  # Access the first worksheet
 
-print(sheet.row_values(1))
+# print(sheet.row_values(1))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import PIL
 
 CARD_WIDTH = 250
 CARD_HEIGHT = 350
+key = "AIzaSyDOsvY1riiBGMfHEN4m0oLA2V4TENxn2OU"
 
 def generate_card(card_data: list):
     #new_card = PIL.Image.new("hsv", (CARD_WIDTH, CARD_HEIGHT), (255, 255, 255))
@@ -10,7 +11,13 @@ def generate_card(card_data: list):
     pass
 
 def main():
+    gc = gspread.api_key("AIzaSyDOsvY1riiBGMfHEN4m0oLA2V4TENxn2OU")
+
+    sh = gc.open_by_key("1RyVuXuTbjReXnPB0BYGq-aMEtmI_Z3HK2iEkngKHFN4")
+
+    print(sh.sheet1.get('A1'))
     print("Hi")
+    
     generate_card([1])
 
 if __name__ == '__main__':
